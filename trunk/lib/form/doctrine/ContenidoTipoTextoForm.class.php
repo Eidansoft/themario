dd562220103d0e4eea7cf86c7fbeb732556bf106
eidansoft <eidansoft@0b83f240-edb9-28ad-825a-14fb3393bf89>
2010-10-25 08:02:22 +0000

    Refactorizado los nombres de las tablas que almacenan los tipos de contenidos Ahor se llaman ContenidoTipoTexto y ContenidoTipoCuestionario

---
+++ trunk/lib/form/doctrine/ContenidoTipoTextoForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * ContenidoTipoTexto form.
+ *
+ * @package    themario
+ * @subpackage form
+ * @author     Alejandro Lorente. eidansoft ARROBA gmail PUNTO com
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class ContenidoTipoTextoForm extends BaseContenidoTipoTextoForm
+{
+  public function configure()
+  {
+  }
+}
