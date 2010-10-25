a3d6d668503d00778b7c21352f723b4ef2147f37
eidansoft <eidansoft@0b83f240-edb9-28ad-825a-14fb3393bf89>
2010-10-25 09:42:48 +0000

    Cambiados los nombres de las tabalas de los tipos de cuestiones a:  - TipoPreguntaAlternativa  - TipoPreguntaCorta

---
+++ lib/filter/doctrine/base/BaseTipoPreguntaAlternativaFormFilter.class.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * TipoPreguntaAlternativa filter form base class.
+ *
+ * @package    themario
+ * @subpackage filter
+ * @author     Alejandro Lorente. eidansoft ARROBA gmail PUNTO com
+ * @version    SVN: $Id: sfDoctrineFormFilterGeneratedTemplate.php 29570 2010-05-21 14:49:47Z Kris.Wallsmith $
+ */
+abstract class BaseTipoPreguntaAlternativaFormFilter extends BaseFormFilterDoctrine
+{
+  public function setup()
+  {
+    $this->setWidgets(array(
+      'cuestion_id' => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('Cuestion'), 'add_empty' => true)),
+      'texto'       => new sfWidgetFormFilterInput(array('with_empty' => false)),
+    ));
+
+    $this->setValidators(array(
+      'cuestion_id' => new sfValidatorDoctrineChoice(array('required' => false, 'model' => $this->getRelatedModelName('Cuestion'), 'column' => 'id')),
+      'texto'       => new sfValidatorPass(array('required' => false)),
+    ));
+
+    $this->widgetSchema->setNameFormat('tipo_pregunta_alternativa_filters[%s]');
+
+    $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
+
+    $this->setupInheritance();
+
+    parent::setup();
+  }
+
+  public function getModelName()
+  {
+    return 'TipoPreguntaAlternativa';
+  }
+
+  public function getFields()
+  {
+    return array(
+      'id'          => 'Number',
+      'cuestion_id' => 'ForeignKey',
+      'texto'       => 'Text',
+    );
+  }
+}
